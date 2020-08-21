8905c101fe07c543c2aaba98ed928adcbad72ac0
Raazesh Sainudiin <raazesh.sainudiin@gmail.com>
2020-08-21 16:02:26 +0200

    draft title added

--- notebooks/db/02streamable-trend-calculus.scala
+++ notebooks/db/02streamable-trend-calculus.scala
@@ -1,4 +1,9 @@
 // Databricks notebook source
+// MAGIC %md
+// MAGIC # Streaming Trend Calculus with Maximum Necessary Reversals
+
+// COMMAND ----------
+
 import java.sql.Timestamp
 import io.delta.tables._
 import org.apache.spark.sql._
