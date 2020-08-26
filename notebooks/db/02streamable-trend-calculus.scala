fb5203ac6e0aade72f3858141a15851a0d8f490f
JohannesGraner <67631091+JohannesGraner@users.noreply.github.com>
2020-08-26 15:04:28 +0200

    Updated to use all available fx1m data.

--- notebooks/db/02streamable-trend-calculus.scala
+++ notebooks/db/02streamable-trend-calculus.scala
@@ -46,13 +46,11 @@ import org.lamastex.spark.trendcalculus._
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC Input data in s3. The data contains oil price data from 2010 to 2019 and gold price data from 2010.
+// MAGIC Input data in s3. The data contains oil price data from 2010 to last month and gold price data from 2009 to last month.
 
 // COMMAND ----------
 
 val rootPath = "s3a://osint-gdelt-reado/canwrite/summerinterns2020/johannes/streamable-trend-calculus/"
-val oilAllPath = rootPath + "oilDataAll"
-val gold2010Path = rootPath + "goldData2010"
 val oilGoldPath = rootPath + "oilGoldDelta"
 
 // COMMAND ----------
@@ -200,7 +198,7 @@ val maxRevDS = revTables.foldLeft(oilGoldTable.toDF.withColumn("reversal", lit(0
 // COMMAND ----------
 
 // Writing the joined dataset to a delta table.
-// maxRevDS.write.format("delta").partitionBy("ticker").save(maxRevPath)
+maxRevDS.write.format("delta").partitionBy("ticker").save(maxRevPath)
 
 // COMMAND ----------
 
